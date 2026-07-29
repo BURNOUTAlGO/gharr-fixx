1d993bd4f9dd29fa5231797d5cad102f85199771
thisi <thisiabhimaurya@gmail.com>
2026-07-29 20:21:06 +0530

    prod 4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import axios from 'axios'
 import { BrowserRouter } from 'react-router-dom'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+const API_URL = import.meta.env.VITE_API_URL;
+
+axios.defaults.baseURL = API_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
